1ad62ffa901e3b614b17cf1c0fc0d7b08c9e99d4
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2023-02-16 14:10:49 -0400

    Funcionalidad Saludar deacuerdo a la edad

--- src/presenter-saludador.js
+++ src/presenter-saludador.js
@@ -3,12 +3,25 @@ import multiplicar from "./multiplicador";*/
 
 const nombre= document.querySelector("#nombre");
 const form = document.querySelector("#saludador-form");
+const genero= document.querySelector("#genero");
+const edad= document.querySelector("#edad");
+//const form2 = dcument.querySelector("#sa-form");
 const div = document.querySelector("#resultado-div");
+
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  //const nombre = Text.Text(first.value);
+  
+  if(genero.value =="femenino" && edad.value >=30){
+    genero.value="SRA.";
+  }
+  if(genero.value =="masculino" && edad.value >=30){
+    genero.value="SR.";
+  } 
+  if (edad.value<30){
+    genero.value="BIENVENIDO";
+  }
 
-  div.innerHTML = "HOLA " + nombre.value;
+  div.innerHTML = "HOLA" + " "+ genero.value + " " + nombre.value  ;
 });
