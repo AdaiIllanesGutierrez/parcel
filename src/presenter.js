5516bfd093aa1c564f0361e83278aaf37b5ccbf0
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2023-02-16 00:10:46 -0400

    tarea multiplicacion version 1

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,15 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+
+const uno = document.querySelector("#numero1");
+const dos= document.querySelector("#numero2");
+const form2 = document.querySelector("#multiplicar-form");
+const div2 = document.querySelector("#resultado-div2");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +18,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(uno.value);
+  const secondNumber = Number.parseInt(dos.value);
+
+  div2.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
