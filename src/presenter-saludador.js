1f8e861047bcc7516cf65735234f9b30030af13e
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2023-02-22 14:13:01 -0400

    funcionalidad Saludador en ingles y espaniol

--- src/presenter-saludador.js
+++ src/presenter-saludador.js
@@ -5,38 +5,53 @@ const nombre= document.querySelector("#nombre");
 const form = document.querySelector("#saludador-form");
 const genero= document.querySelector("#genero");
 const edad= document.querySelector("#edad");
+const idioma= document.querySelector("#idioma");
 //const form2 = dcument.querySelector("#sa-form");
 const div = document.querySelector("#resultado-div");
 
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  
+  var gen;
+  var gen2;
   if(genero.value =="femenino" && edad.value >=30){
-    genero.value="SRA.";
+    gen="SRA.";
+    gen2="Ms.";
   }
   if(genero.value =="masculino" && edad.value >=30){
-    genero.value="SR.";
+    gen="SR.";
+    gen2="Mrs.";
   } 
   if (edad.value<30){
-    genero.value="BIENVENIDO";
+    genero.value="";
   }
   // crea un nuevo objeto `Date`
 var today = new Date();
 // obtener la fecha y la hora
 var now = today.getHours();
+var now2;
 var hora=parseInt(now);
 if (hora>=0 && hora <12){
-now=" Buenos Dias"
-}if(hora >=12 && hora <7){
+now=" Buenos Dias";
+now2="Good Morning";
+}if(hora >=12 && hora <19){
   now= "Buenas Tardes"
+  now2="Good Afternoon";
 }
-if(hora >=7 && hora <24){
-  now= "Buenas Noches"
+if(hora >=19 && hora <24){
+  now= "Buenas Noches";
+  now2="Good Night";
+  
 }
+if(idioma.value =="es"){
+  div.innerHTML = "Hola" + " "+ gen + " " + nombre.value + " " + now  ;
+}
+if(idioma.value =="en"){
+  div.innerHTML = "Hello" + " "+ gen2+ " " + nombre.value + " " + now2  ;
+}
+
 
 console.log(now);
 
-  div.innerHTML = "HOLA" + " "+ genero.value + " " + nombre.value + " " + now  ;
+  
 });
